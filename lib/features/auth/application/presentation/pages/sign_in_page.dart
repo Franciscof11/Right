2a5c273ed23d74d0a4ef74b4421b957324759b51
Francisco Gabriel <franciscogabrielf11@gmail.com>
2023-12-04 17:30:33 -0300

    refac: auth pages

--- lib/features/auth/application/presentation/pages/sign_in_page.dart
+++ lib/features/auth/application/presentation/pages/sign_in_page.dart
@@ -18,166 +18,164 @@ class _SignInPageState extends State<SignInPage> {
       canPop: false,
       child: Scaffold(
         backgroundColor: LightColors().primaryContainer,
-        body: SafeArea(
-          child: SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 22),
-              child: Column(
-                children: [
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  SvgPicture.asset(
-                    'assets/imgs/logo.svg',
-                    width: 90,
-                    height: 90,
-                  ),
-                  const SizedBox(
-                    height: 50,
-                  ),
-                  Material(
-                    elevation: 12,
-                    borderRadius: BorderRadius.circular(12),
-                    child: Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(12),
-                        color: Colors.white,
-                      ),
-                      child: Column(
-                        children: [
-                          const SizedBox(
-                            height: 35,
-                          ),
-                          Text.rich(
-                            TextSpan(
-                                text: 'Insira seu login ',
-                                style: GoogleFonts.raleway(
-                                  textStyle: TextStyle(
-                                    color: LightColors().primary,
-                                    fontSize: 24,
-                                    fontWeight: FontWeight.w600,
-                                  ),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 22),
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 60,
+                ),
+                SvgPicture.asset(
+                  'assets/imgs/logo.svg',
+                  width: 90,
+                  height: 90,
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
+                Material(
+                  elevation: 12,
+                  borderRadius: BorderRadius.circular(12),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      color: Colors.white,
+                    ),
+                    child: Column(
+                      children: [
+                        const SizedBox(
+                          height: 35,
+                        ),
+                        Text.rich(
+                          TextSpan(
+                              text: 'Insira seu login ',
+                              style: GoogleFonts.raleway(
+                                textStyle: TextStyle(
+                                  color: LightColors().primary,
+                                  fontSize: 24,
+                                  fontWeight: FontWeight.w600,
                                 ),
-                                children: [
-                                  TextSpan(
-                                      text: 'right',
-                                      style: TextStyle(
-                                        color: LightColors().onPrimaryContainer,
-                                      ))
-                                ]),
-                          ),
-                          const SizedBox(
-                            height: 35,
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.symmetric(horizontal: 20),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
+                              ),
                               children: [
-                                CustomTextFormField(
-                                  controller: TextEditingController(),
-                                  label: 'E-mail',
-                                  type: FormTypes.email,
-                                ),
-                                const SizedBox(
-                                  height: 20,
-                                ),
-                                CustomTextFormField(
-                                  controller: TextEditingController(),
-                                  label: 'Senha',
-                                  type: FormTypes.password,
-                                  isSecret: true,
-                                ),
-                                const SizedBox(
-                                  height: 14,
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.only(right: 10),
-                                  child: Align(
-                                    alignment: Alignment.centerRight,
-                                    child: GestureDetector(
-                                      onTap: () {
-                                        Navigator.pushNamed(
-                                            context, '/RecoverPasswordPage');
-                                      },
-                                      child: Text(
-                                        'Esqueceu a senha?',
-                                        style: GoogleFonts.raleway(
-                                          color: LightColors().primary,
-                                          fontSize: 14,
-                                          fontWeight: FontWeight.w800,
-                                        ),
+                                TextSpan(
+                                    text: 'right',
+                                    style: TextStyle(
+                                      color: LightColors().onPrimaryContainer,
+                                    ))
+                              ]),
+                        ),
+                        const SizedBox(
+                          height: 35,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 20),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              CustomTextFormField(
+                                controller: TextEditingController(),
+                                label: 'E-mail',
+                                type: FormTypes.email,
+                              ),
+                              const SizedBox(
+                                height: 20,
+                              ),
+                              CustomTextFormField(
+                                controller: TextEditingController(),
+                                label: 'Senha',
+                                type: FormTypes.password,
+                                isSecret: true,
+                              ),
+                              const SizedBox(
+                                height: 14,
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(right: 10),
+                                child: Align(
+                                  alignment: Alignment.centerRight,
+                                  child: GestureDetector(
+                                    onTap: () {
+                                      Navigator.pushNamed(
+                                          context, '/RecoverPasswordPage');
+                                    },
+                                    child: Text(
+                                      'Esqueceu a senha?',
+                                      style: GoogleFonts.raleway(
+                                        color: LightColors().primary,
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.w800,
                                       ),
                                     ),
                                   ),
                                 ),
-                                const SizedBox(
-                                  height: 25,
+                              ),
+                              const SizedBox(
+                                height: 25,
+                              ),
+                              Text(
+                                'Não possui conta?',
+                                style: GoogleFonts.raleway(
+                                  height: 0,
+                                  color: LightColors().onPrimaryContainer,
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w800,
                                 ),
-                                Text(
-                                  'Não possui conta?',
+                              ),
+                              const SizedBox(height: 10),
+                              GestureDetector(
+                                onTap: () {
+                                  Navigator.pushNamed(context, '/SignUpPage');
+                                },
+                                child: Text(
+                                  'Cadastre-se',
                                   style: GoogleFonts.raleway(
-                                    height: 0,
-                                    color: LightColors().onPrimaryContainer,
-                                    fontSize: 14,
+                                    color: LightColors().primary,
+                                    fontSize: 20,
                                     fontWeight: FontWeight.w800,
                                   ),
                                 ),
-                                const SizedBox(height: 10),
-                                GestureDetector(
-                                  onTap: () {
-                                    Navigator.pushNamed(context, '/SignUpPage');
-                                  },
+                              ),
+                              const SizedBox(
+                                height: 32,
+                              ),
+                              Align(
+                                alignment: Alignment.center,
+                                child: ElevatedButton(
+                                  onPressed: () {},
+                                  style: ElevatedButton.styleFrom(
+                                    shape: ContinuousRectangleBorder(
+                                      borderRadius: BorderRadius.circular(20),
+                                    ),
+                                    padding: const EdgeInsets.symmetric(
+                                      vertical: 15,
+                                      horizontal: 80,
+                                    ),
+                                    backgroundColor:
+                                        LightColors().onPrimaryContainer,
+                                  ),
                                   child: Text(
-                                    'Cadastre-se',
+                                    'Entrar',
                                     style: GoogleFonts.raleway(
-                                      color: LightColors().primary,
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.w800,
+                                      color: Colors.white,
+                                      fontSize: 26,
+                                      fontWeight: FontWeight.w600,
                                     ),
                                   ),
                                 ),
-                                const SizedBox(
-                                  height: 32,
-                                ),
-                                Align(
-                                  alignment: Alignment.center,
-                                  child: ElevatedButton(
-                                    onPressed: () {},
-                                    style: ElevatedButton.styleFrom(
-                                      shape: ContinuousRectangleBorder(
-                                        borderRadius: BorderRadius.circular(20),
-                                      ),
-                                      padding: const EdgeInsets.symmetric(
-                                        vertical: 15,
-                                        horizontal: 80,
-                                      ),
-                                      backgroundColor:
-                                          LightColors().onPrimaryContainer,
-                                    ),
-                                    child: Text(
-                                      'Entrar',
-                                      style: GoogleFonts.raleway(
-                                        color: Colors.white,
-                                        fontSize: 26,
-                                        fontWeight: FontWeight.w600,
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                                const SizedBox(height: 35),
-                              ],
-                            ),
-                          )
-                        ],
-                      ),
+                              ),
+                              const SizedBox(height: 35),
+                            ],
+                          ),
+                        )
+                      ],
                     ),
                   ),
-                  const SizedBox(
-                    height: 50,
-                  ),
-                ],
-              ),
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
+              ],
             ),
           ),
         ),
--- lib/features/auth/application/presentation/pages/sign_up_page.dart
+++ lib/features/auth/application/presentation/pages/sign_up_page.dart
@@ -15,160 +15,158 @@ class _SignUpPageState extends State<SignUpPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: LightColors().primaryContainer,
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Column(
-              children: [
-                const SizedBox(
-                  height: 20,
-                ),
-                Row(
-                  children: [
-                    GestureDetector(
-                      onTap: () => Navigator.pop(context),
-                      child: Icon(
-                        Icons.arrow_circle_left_outlined,
-                        color: LightColors().onPrimaryContainer,
-                        size: 38,
-                      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 40,
+              ),
+              Row(
+                children: [
+                  GestureDetector(
+                    onTap: () => Navigator.pop(context),
+                    child: Icon(
+                      Icons.arrow_circle_left_outlined,
+                      color: LightColors().onPrimaryContainer,
+                      size: 38,
                     ),
-                    const SizedBox(
-                      width: 10,
-                    ),
-                    Text(
-                      'Voltar',
-                      style: GoogleFonts.raleway(
-                        color: LightColors().onPrimaryContainer,
-                        fontSize: 22,
-                        fontWeight: FontWeight.w800,
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    'Crie sua conta',
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    'Voltar',
                     style: GoogleFonts.raleway(
                       color: LightColors().onPrimaryContainer,
-                      fontSize: 28,
+                      fontSize: 22,
+                      fontWeight: FontWeight.w800,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'Crie sua conta',
+                  style: GoogleFonts.raleway(
+                    color: LightColors().onPrimaryContainer,
+                    fontSize: 28,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    child: CustomTextFormField(
+                      controller: TextEditingController(),
+                      label: 'Nome',
+                      type: FormTypes.generic,
+                    ),
+                  ),
+                  const SizedBox(width: 20),
+                  Expanded(
+                    child: CustomTextFormField(
+                      controller: TextEditingController(),
+                      label: 'Sobrenome',
+                      type: FormTypes.generic,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              CustomTextFormField(
+                controller: TextEditingController(),
+                label: 'E-mail',
+                type: FormTypes.email,
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              CustomTextFormField(
+                controller: TextEditingController(),
+                label: 'Senha',
+                type: FormTypes.password,
+                isSecret: true,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    softWrap: true,
+                    textAlign: TextAlign.start,
+                    '• Mínimo de 6 caracteres.',
+                    style: GoogleFonts.raleway(
+                      color: LightColors().onPrimaryContainer,
+                      fontSize: 14,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  const SizedBox(height: 6),
+                  Text(
+                    softWrap: true,
+                    textAlign: TextAlign.start,
+                    '• Use letras maiúsculas, minúsculas, números e símbolos.',
+                    style: GoogleFonts.raleway(
+                      color: LightColors().onPrimaryContainer,
+                      fontSize: 14,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              CustomTextFormField(
+                controller: TextEditingController(),
+                label: 'Confirmar Senha',
+                type: FormTypes.password,
+                isSecret: true,
+              ),
+              const SizedBox(height: 60),
+              Align(
+                alignment: Alignment.center,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/SucessfulRegistration');
+                  },
+                  style: ElevatedButton.styleFrom(
+                    shape: ContinuousRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 15,
+                      horizontal: 40,
+                    ),
+                    backgroundColor: LightColors().onPrimaryContainer,
+                  ),
+                  child: Text(
+                    'Cadastrar',
+                    style: GoogleFonts.raleway(
+                      color: Colors.white,
+                      fontSize: 24,
                       fontWeight: FontWeight.w600,
                     ),
                   ),
                 ),
-                const SizedBox(
-                  height: 30,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Expanded(
-                      child: CustomTextFormField(
-                        controller: TextEditingController(),
-                        label: 'Nome',
-                        type: FormTypes.generic,
-                      ),
-                    ),
-                    const SizedBox(width: 20),
-                    Expanded(
-                      child: CustomTextFormField(
-                        controller: TextEditingController(),
-                        label: 'Sobrenome',
-                        type: FormTypes.generic,
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 30,
-                ),
-                CustomTextFormField(
-                  controller: TextEditingController(),
-                  label: 'E-mail',
-                  type: FormTypes.email,
-                ),
-                const SizedBox(
-                  height: 30,
-                ),
-                CustomTextFormField(
-                  controller: TextEditingController(),
-                  label: 'Senha',
-                  type: FormTypes.password,
-                  isSecret: true,
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      softWrap: true,
-                      textAlign: TextAlign.start,
-                      '• Mínimo de 6 caracteres.',
-                      style: GoogleFonts.raleway(
-                        color: LightColors().onPrimaryContainer,
-                        fontSize: 14,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                    const SizedBox(height: 6),
-                    Text(
-                      softWrap: true,
-                      textAlign: TextAlign.start,
-                      '• Use letras maiúsculas, minúsculas, números e símbolos.',
-                      style: GoogleFonts.raleway(
-                        color: LightColors().onPrimaryContainer,
-                        fontSize: 14,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 30,
-                ),
-                CustomTextFormField(
-                  controller: TextEditingController(),
-                  label: 'Confirmar Senha',
-                  type: FormTypes.password,
-                  isSecret: true,
-                ),
-                const SizedBox(height: 60),
-                Align(
-                  alignment: Alignment.center,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/SucessfulRegistration');
-                    },
-                    style: ElevatedButton.styleFrom(
-                      shape: ContinuousRectangleBorder(
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                      padding: const EdgeInsets.symmetric(
-                        vertical: 15,
-                        horizontal: 40,
-                      ),
-                      backgroundColor: LightColors().onPrimaryContainer,
-                    ),
-                    child: Text(
-                      'Cadastrar',
-                      style: GoogleFonts.raleway(
-                        color: Colors.white,
-                        fontSize: 24,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 35),
-              ],
-            ),
+              ),
+              const SizedBox(height: 35),
+            ],
           ),
         ),
       ),
