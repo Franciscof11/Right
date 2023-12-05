482a003ce2b1fe037cecae282920e46b04d7d87b
Francisco Gabriel <franciscogabrielf11@gmail.com>
2023-12-05 11:52:42 -0300

    fix: textFormField bug

--- lib/features/auth/application/presentation/pages/recover_password_page.dart
+++ lib/features/auth/application/presentation/pages/recover_password_page.dart
@@ -12,6 +12,8 @@ class RecoverPasswordPage extends StatefulWidget {
 }
 
 class _RecoverPasswordPageState extends State<RecoverPasswordPage> {
+  final formKey = GlobalKey<FormState>();
+  final emailController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,173 +37,176 @@ class _RecoverPasswordPageState extends State<RecoverPasswordPage> {
                 ),
                 child: Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 20),
-                  child: Column(
-                    children: [
-                      const SizedBox(
-                        height: 40,
-                      ),
-                      Row(
-                        children: [
-                          GestureDetector(
-                            onTap: () => Navigator.pop(context),
-                            child: Icon(
-                              Icons.arrow_circle_left_outlined,
-                              color: LightColors().onPrimaryContainer,
-                              size: 38,
-                            ),
-                          ),
-                          const SizedBox(
-                            width: 10,
-                          ),
-                          Text(
-                            'Recuperar Senha',
-                            style: GoogleFonts.raleway(
-                              color: LightColors().onPrimaryContainer,
-                              fontSize: 28,
-                              fontWeight: FontWeight.w600,
-                            ),
-                          ),
-                        ],
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      Text.rich(
-                        TextSpan(
-                          text: 'Ops!',
-                          style: GoogleFonts.raleway(
-                            color: LightColors().primary,
-                            fontSize: 22,
-                            fontWeight: FontWeight.w700,
-                          ),
+                  child: Form(
+                    key: formKey,
+                    child: Column(
+                      children: [
+                        const SizedBox(
+                          height: 50,
+                        ),
+                        Row(
                           children: [
-                            TextSpan(
-                              text:
-                                  ' Esqueceu a senha? Estamos aqui para te ajudar a recuperar o acesso à sua conta.',
-                              style: GoogleFonts.raleway(
-                                color: DarkColors().primaryContainer,
-                                fontSize: 16,
-                                fontWeight: FontWeight.w500,
+                            GestureDetector(
+                              onTap: () => Navigator.pop(context),
+                              child: Icon(
+                                Icons.arrow_circle_left_outlined,
+                                color: LightColors().onPrimaryContainer,
+                                size: 38,
                               ),
-                            )
+                            ),
+                            const SizedBox(
+                              width: 10,
+                            ),
+                            Text(
+                              'Recuperar Senha',
+                              style: GoogleFonts.raleway(
+                                color: LightColors().onPrimaryContainer,
+                                fontSize: 28,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
                           ],
                         ),
-                      ),
-                      const SizedBox(
-                        height: 30,
-                      ),
-                      SvgPicture.asset(
-                        'assets/imgs/forgot_password.svg',
-                        width: MediaQuery.of(context).size.width,
-                        height: 240,
-                      ),
-                      const SizedBox(
-                        height: 25,
-                      ),
-                      Align(
-                        alignment: Alignment.centerLeft,
-                        child: Text.rich(
-                          TextSpan(
-                            text: 'Insira seu ',
-                            style: GoogleFonts.raleway(
-                              color: LightColors().onPrimaryContainer,
-                              fontSize: 24,
-                              fontWeight: FontWeight.w600,
-                            ),
-                            children: [
-                              TextSpan(
-                                text: 'E-mail ',
-                                style: GoogleFonts.raleway(
-                                  color: LightColors().onPrimaryContainer,
-                                  fontSize: 24,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              TextSpan(
-                                text: 'abaixo',
-                                style: GoogleFonts.raleway(
-                                  color: LightColors().onPrimaryContainer,
-                                  fontSize: 24,
-                                  fontWeight: FontWeight.w600,
-                                ),
-                              ),
-                            ],
-                          ),
+                        const SizedBox(
+                          height: 20,
                         ),
-                      ),
-                      const SizedBox(height: 2),
-                      Align(
-                        alignment: Alignment.centerLeft,
-                        child: Text.rich(
+                        Text.rich(
                           TextSpan(
-                            text: 'enviaremos um ',
+                            text: 'Ops!',
                             style: GoogleFonts.raleway(
-                              color: LightColors().onPrimaryContainer,
-                              fontSize: 14,
-                              fontWeight: FontWeight.w500,
+                              color: LightColors().primary,
+                              fontSize: 22,
+                              fontWeight: FontWeight.w700,
                             ),
                             children: [
-                              TextSpan(
-                                text: 'e-mail de renovação de senha',
-                                style: GoogleFonts.raleway(
-                                  color: LightColors().onPrimaryContainer,
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
                               TextSpan(
                                 text:
-                                    ' para o seu endereço de email cadastrado.',
+                                    ' Esqueceu a senha? Estamos aqui para te ajudar a recuperar o acesso à sua conta.',
                                 style: GoogleFonts.raleway(
-                                  color: LightColors().onPrimaryContainer,
-                                  fontSize: 14,
+                                  color: DarkColors().primaryContainer,
+                                  fontSize: 16,
                                   fontWeight: FontWeight.w500,
                                 ),
-                              ),
+                              )
                             ],
                           ),
                         ),
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      CustomTextFormField(
-                        controller: TextEditingController(),
-                        label: 'E-mail',
-                        type: FormTypes.email,
-                      ),
-                      const SizedBox(
-                        height: 30,
-                      ),
-                      Align(
-                        alignment: Alignment.center,
-                        child: ElevatedButton(
-                          onPressed: () {
-                            Navigator.pushNamed(
-                                context, '/SuccessRecoverEmail');
-                          },
-                          style: ElevatedButton.styleFrom(
-                            shape: ContinuousRectangleBorder(
-                              borderRadius: BorderRadius.circular(20),
-                            ),
-                            padding: const EdgeInsets.symmetric(
-                              vertical: 15,
-                              horizontal: 40,
-                            ),
-                            backgroundColor: LightColors().onPrimaryContainer,
-                          ),
-                          child: Text(
-                            'Enviar',
-                            style: GoogleFonts.raleway(
-                              color: Colors.white,
-                              fontSize: 24,
-                              fontWeight: FontWeight.w600,
+                        const SizedBox(
+                          height: 15,
+                        ),
+                        SvgPicture.asset(
+                          'assets/imgs/forgot_password.svg',
+                          width: MediaQuery.of(context).size.width,
+                          height: 180,
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text.rich(
+                            TextSpan(
+                              text: 'Insira seu ',
+                              style: GoogleFonts.raleway(
+                                color: LightColors().onPrimaryContainer,
+                                fontSize: 24,
+                                fontWeight: FontWeight.w600,
+                              ),
+                              children: [
+                                TextSpan(
+                                  text: 'E-mail ',
+                                  style: GoogleFonts.raleway(
+                                    color: LightColors().onPrimaryContainer,
+                                    fontSize: 24,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                TextSpan(
+                                  text: 'abaixo',
+                                  style: GoogleFonts.raleway(
+                                    color: LightColors().onPrimaryContainer,
+                                    fontSize: 24,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                              ],
                             ),
                           ),
                         ),
-                      ),
-                      const SizedBox(height: 35),
-                    ],
+                        const SizedBox(height: 2),
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text.rich(
+                            TextSpan(
+                              text: 'enviaremos um ',
+                              style: GoogleFonts.raleway(
+                                color: LightColors().onPrimaryContainer,
+                                fontSize: 14,
+                                fontWeight: FontWeight.w500,
+                              ),
+                              children: [
+                                TextSpan(
+                                  text: 'e-mail de renovação de senha',
+                                  style: GoogleFonts.raleway(
+                                    color: LightColors().onPrimaryContainer,
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                TextSpan(
+                                  text:
+                                      ' para o seu endereço de email cadastrado.',
+                                  style: GoogleFonts.raleway(
+                                    color: LightColors().onPrimaryContainer,
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        CustomTextFormField(
+                          controller: emailController,
+                          label: 'E-mail',
+                          type: FormTypes.email,
+                        ),
+                        const SizedBox(
+                          height: 30,
+                        ),
+                        Align(
+                          alignment: Alignment.center,
+                          child: ElevatedButton(
+                            onPressed: () {
+                              Navigator.pushNamed(
+                                  context, '/SuccessRecoverEmail');
+                            },
+                            style: ElevatedButton.styleFrom(
+                              shape: ContinuousRectangleBorder(
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                              padding: const EdgeInsets.symmetric(
+                                vertical: 15,
+                                horizontal: 40,
+                              ),
+                              backgroundColor: LightColors().onPrimaryContainer,
+                            ),
+                            child: Text(
+                              'Enviar',
+                              style: GoogleFonts.raleway(
+                                color: Colors.white,
+                                fontSize: 24,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 35),
+                      ],
+                    ),
                   ),
                 ),
               ),
